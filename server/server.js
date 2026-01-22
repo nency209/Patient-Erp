b82639ef2c7ec1fb811ef81de6a385326e6f5fb2
Nency Dholariya <nencydholariya173@gmail.com>
2026-01-22 18:07:05 +0530

    Force remove client/node_modules from git

--- server/server.js
+++ server/server.js
@@ -14,13 +14,13 @@ const app = express();
 
 
 
-app.use(cors())
+// app.use(cors())
 
-// app.use(cors({
-//   origin: 'https://patient-erp.vercel.app', 
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"] // Explicitly allow all methods
-// }));
+app.use(cors({
+  origin: 'https://patient-erp.vercel.app', 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"] // Explicitly allow all methods
+}));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
